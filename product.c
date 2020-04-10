9a8c82393a8cd588de242acad276a030bac0112c
Lim yeonkyu <21900613@handong.edu>
2020-04-11 00:13:40 +0900

    change selectMenu function

--- product.c
+++ product.c
@@ -43,6 +43,7 @@ int selectMenu(){
 	printf("3. 수정\n");
 	printf("4. 삭제\n");
 	printf("5. 제품명으로 검색\n");
+	printf("6. 별점으로 검색\n");
 	printf("0. 종료\n\n");
 	printf("=> 원하는 메뉴는?");
 	scanf("%d",&menu);
