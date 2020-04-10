3b970b0495b24d76456089d959f93d810509dd38
Lim yeonkyu <21900613@handong.edu>
2020-04-10 22:12:27 +0900

    create main.c

---
+++ main.c
@@ -0,0 +1,65 @@
+#include "manager.h"
+int main(void){
+        count=0;
+	curcount=0;
+        while (1){
+#ifdef DEBUG
+printf("debug [main.c]: call selectMenu()\n");
+#endif
+                menu=selectMenu();
+                if(menu==0) break;
+                if(menu==1){
+                        if(count>0){
+#ifdef DEBUG
+printf("debug [main.c]: call listProduct()\n");
+#endif
+                                listProduct(plist,curcount);
+			}
+                        else
+                                printf("데이터가 없습니다!\n");
+                }
+                else if(menu==2){
+#ifdef DEBUG
+printf("debug [main.c]: call addProduct()\n");
+#endif
+                                addProduct(&plist[curcount++]);
+                                count++;
+                }
+                else if(menu==3){
+#ifdef DEBUG
+printf("debug [main.c]: call selectDataNo()\n");
+#endif
+                        int no=selectDataNo(plist,curcount);
+			if(no==0){
+				printf("=>취소됨\n");
+				continue;
+			}
+#ifdef DEBUG
+printf("debug [main.c]: call updateProduct()\n");
+#endif
+			updateProduct(&plist[no-1]);
+		}
+                else if(menu==4){
+#ifdef DEBUG
+printf("debug [main.c]: call selectDataNo()\n");
+#endif
+			int no=selectDataNo(plist,curcount);
+			if(no==0){
+				printf("=> 취소됨!\n");
+				continue;
+			}
+			int deleteok;
+			printf("정말로 삭제하시겠습니까?(삭제:1)");
+			scanf("%d",&deleteok);
+			if(deleteok==1){
+#ifdef DEBUG
+printf("debug [main.c]: call deleteProduct()\n");
+#endif
+				deleteProduct(&plist[no-1]);
+				count--;
+			}
+		}
+        }
+        printf("종료됨!\n");
+        return 0;
+}
