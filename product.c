ddf4678f158e2af7ad36d7f0c7781e77b2e10608
Lim yeonkyu <21900613@handong.edu>
2020-04-11 20:30:10 +0900

    make loadData and saveData function in product.c

--- product.c
+++ product.c
@@ -108,3 +108,34 @@ void searchStdprice(Product *p,int count){
         }
     }
 }
+
+void saveData(Product *p, int count){
+	FILE *fp;
+	fp=fopen("product.txt","wt");
+		for(int i=0;i<count;i++){
+			if(p[i].price!=-1){
+				fprintf("%d   %d    %d        %d   %s",p[i].weight,p[i].price,p[i].stdPrice,p[i].star,p[i].name);
+			}
+		}
+	fclose(fp);
+	printf("저장됨!");
+}
+
+int loadData(Product p[]){
+    int count =0;
+    FILE *fp;
+    fp=fopen("product.txt","rt");
+    if(fp==NULL){
+        printf("=> 파일 없음\n");
+        return 0;
+    }
+    for(; ; count++){
+        fscanf(fp,"%d %d %d %d",&p[i].weight,&p[i].price,&p[i].stdPrice,&p[i].star);
+        fgets(p[count].name,sizeof(p[count].name),fp);
+        if(feof(fp))
+            break;
+    }
+    fclose(fp);
+    printf("=> 로딩 성공!\n");
+    return count;
+}
